19ad5527f7a3ba9f8a90d828b82268b14614d128
ashishodia <ankur.shishodia@gmail.com>
2016-11-10 19:30:02 +0530

    code added

--- es_mongo_sync/export_packages_daily.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# example of using arguments to a script
-echo "mongo dump"
-
-mongoexport --host 172.0.8.35 --db delhivery_db -c packages --csv --fields wbn,pd,pt,pdd,cs.st,cs.ss,date.frd,dd.fdd --query "$(python /data1/delhivery/es_mongo_sync/pmongexp_packages_bck.py)" --out "/data1/delhivery/es_mongo_sync/package_data.csv" -u ro_express -p 'x[td7R%;,'
-
-echo "mongo dump is over" 
